3bae416024dcee3f00f4ebc6f33e3c6ba95cb722
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2020-01-30 09:46:25 -0500

    made functions at the bottom of the Main class to clean code in the main method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,58 +8,39 @@ import static com.company.Shape.afterShapeDisplayDecision;
 
 public class Main
 {
-    //TODO: commit with this progress
-    // Organized project into seperate packages+Class files
-    //  Added ShapeActions interface
-    //   fixed errors to start looking for bugs in console window
-    //    Commented out certain shapes and left
-    //      Created more methods in the shape class to add modularity
-    //       Made big push on box class
-
-
     //TODO: Create Test
     public static void main(String[] args)
     {
-        //Messages to user---------------------------------------------------------------
-        //final because the message should remain const
-        final String greeting = "Hello, Welcome To Your Shape Generator! \n\n " +
-                "Press Any Key To Continue";
-        System.out.println(greeting);
-
-        //Clearing the console-----------------------------------------------------------
-        try
-        {
-            System.in.read();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        System.console().flush();
-
-
-        //Displaying possible shapes to user--------------------------------------------
-
+        greetUser();
+        clearMyConsole();
+        //Displaying possible shapes to user-----------------------------------
         Shape DisplayShape = new Shape();
-        //TODO: Try to put two lines of code in one
-        //Attempt 1
         DisplayShape.getDisplayShape().drawThisShape(DisplayShape.grabSizeParameters());
-        //DisplayShape.drawThisShape(DisplayShape.grabSizeParameters());
-
-
-        //Asking user to go Back to the main screen or exit (RECURSION)-----------------
-        //Waiting 3 seconds after Shape is displayed and display message----------------
-        //TODO: Multithread this with
-
-        try
-        {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-
+        pauseMyConsoleOperations();
+        //Asking user to go Back to the main screen or exit (RECURSION)--------
         afterShapeDisplayDecision();
     }
 
-
+    //------------------------------------------------------------------------------------------------------------------
+    private static  void greetUser()
+    {
+        //Message to user---------------------------------------------------------------
+        //final because the message should remain const
+        final String greeting = "Hello, Welcome To Your Shape Generator! \n\n " + "Press Any Key To Continue";
+        System.out.println(greeting);
+    }
+    private static void clearMyConsole()
+    {
+        //Clearing the console-----------------------------------------------------------
+        try { System.in.read(); }
+        catch (IOException e) { e.printStackTrace(); }
+        System.console().flush();
+    }
+    private static void pauseMyConsoleOperations()
+    {
+        //Waiting 3 seconds after Shape is displayed and display message----------------
+        //TODO: Multithread this with
+        try { TimeUnit.SECONDS.sleep(3); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+    }
 }
